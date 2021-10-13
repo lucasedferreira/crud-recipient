de86c6ba285322a30ddd59da10347116a353a00f
Lucas Ferreira <iuk@pop-os.localdomain>
2021-10-13 01:10:24 -0300

    Corrigido relação de banks com recipients

--- database/models/bank.js
+++ database/models/bank.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Bank.hasMany(models.Recipient, {as: 'recipients'});
+      Bank.hasMany(models.Recipient, {foreignKey: 'bankId'});
     }
   };
   Bank.init({
